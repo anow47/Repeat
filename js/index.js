7e26f60b0187d558f60588429c2dbdb16fec37ed
Hamouda <anow47@yahoo.com>
2024-02-01 16:47:41 +0200

    added a counter to the stats section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 
 const expandMore = document.querySelector('.expand-more')
 const dropdown = document.querySelector('.dropdown')
+const closeIcon = document.querySelector('.close-icon')
+const navBar = document.querySelector('.nav-bar')
+
+
+document.querySelector('.burger').addEventListener('click', () => {
+    toggleMenu(navBar)
+    toggleMenu(closeIcon)
+})
 document.querySelector('.expand-icon-container').addEventListener('click',  () => {
     toggleMenu(expandMore)
 })
@@ -9,20 +17,14 @@ dropdown.addEventListener('click',  () => {
     toggleMenu(subMenuNav)
 })
 
-const navBar = document.querySelector('.nav-bar')
-const closeIcon = document.querySelector('.close-icon')
-document.querySelector('.burger').addEventListener('click', () => {
-    toggleMenu(navBar)
-    toggleMenu(closeIcon)
-})
-
 closeIcon.addEventListener('click', () => {
-    navBar.style.display = 'none'
-    closeIcon.style.display = 'none'
+    navBar.style.visibility = 'hidden'
+    closeIcon.style.visibility = 'hidden'
 })
 function toggleMenu(element){
-    element.style.display = element.style.display === 'none'? 'block' : 'none'
+    element.style.visibility = (element.style.visibility === 'hidden')? 'visible' : 'hidden'
 }
+
 
 const expandIconContainer = document.querySelectorAll('.expand-icon-container')
 const expandIcon = document.querySelectorAll('.expand-icon')
@@ -41,6 +43,16 @@ expandIconContainer[1].addEventListener('click', () => {
 //     {}
 // ]
 
+// Add active class to active link and visited
+const currentLink = document.querySelectorAll('.nav-bar .main-link')
+currentLink.forEach(link => {
+    link.addEventListener('click', () => {
+        currentLink.forEach(el => {
+            el.classList.remove('active')
+        })
+        link.classList.add('active')
+    })
+}) 
 
 //Animation on scroll element fade-in
 const scrollElements = document.querySelectorAll(".js-scroll");
@@ -84,5 +96,43 @@ const handleScrollAnimation = () => {
     window.removeEventListener("scroll", handleScrollAnimation);
   }
 };
-
 window.addEventListener("scroll", handleScrollAnimation);
+
+// Counter on Stats
+let winHeight = window.innerHeight;
+    let ele = document.querySelector(".card");
+    let eleOffsetTop = ele.offsetTop;
+    let eleTop = eleOffsetTop - winHeight;
+    let current = 0;
+
+    function updateCounter($this, countTo) {
+      $this.innerText = Math.floor(countTo);
+    }
+
+    function handleScroll() {
+      let scrollTop = window.scrollY;
+
+      if (current === 0 && scrollTop >= eleTop) {
+        let counters = document.querySelectorAll('.card .count');
+        
+        counters.forEach(function (counter) {
+          let countTo = counter.getAttribute('data-count');
+          let currentCount = 0;
+
+          function animateCounter() {
+            currentCount++;
+            updateCounter(counter, currentCount);
+
+            if (currentCount < countTo) {
+              requestAnimationFrame(animateCounter);
+            } else {
+              updateCounter(counter, countTo);
+            }
+          }
+          animateCounter();
+        });
+        current = 1;
+      }
+    }
+    // Add scroll event listener
+    window.addEventListener('scroll', handleScroll);
